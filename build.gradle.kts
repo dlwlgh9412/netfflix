fa4715a73a567158c01c3774a03fa313621fba5d
dlwlgh9412 <dlwlgh9412@gmail.com>
2023-10-24 19:22:08 +0900

    build.gradle.kts buildscript 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,13 @@ plugins {
     id("io.spring.dependency-management") version "1.0.15.RELEASE"
 }
 
+buildscript {
+    dependencies {
+        classpath("org.springframework.boot:spring-boot-gradle-plugin:3.1.4")
+    }
+}
+
+
 group = "com.copago"
 version = "0.0.1-SNAPSHOT"
 
