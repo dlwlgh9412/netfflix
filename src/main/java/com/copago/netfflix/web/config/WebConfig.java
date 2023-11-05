761ce1432a42ee7fd70600f4b40381321c1b8d12
dlwlgh9412 <dlwlgh9412@gmail.com>
2023-11-05 19:09:54 +0900

    no message

--- src/main/java/com/copago/netfflix/web/config/WebConfig.java
+++ src/main/java/com/copago/netfflix/web/config/WebConfig.java
@@ -4,6 +4,7 @@ import com.copago.netfflix.web.resolver.UserInfoArgumentResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -15,5 +16,15 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(userInfoArgumentResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
